5a3740bff90429d8dfba1d5c4035762ed6178372
Ben <web@pilchbarnet.com>
2019-10-24 13:37:24 -0500

    Added the GA code calls to the popup

--- content.js
+++ content.js
@@ -1,35 +1,48 @@
 
 function DOMtoString(document_root) {
     var html = '',
-        node = document_root.firstChild;
+		node = document_root.firstChild;
+	var city = "";
+
     while (node) {
         html += node.outerHTML;
-        node = node.nextSibling;
-    }
-    var city = "";
-            switch (true) {
-	    case html.includes("UA-9296528-5"):
+		node = node.nextSibling;
+	}
+
+	function getValueByKey(node) {
+		var regex = /^.*UA-.*$/gimu;
+		var match = regex.exec(node);
+		for (let v of node) {
+			v += regex.exec(node);
+			return match + '\n' + v + "\n Not at all sure where the \"u\" got here"
+		}
+	}
+	
+	var googleCodeLines = getValueByKey(html);
+	
+	switch (true) {
+		case html.includes("UA-9296528-5"):
 			city += 'Middleton';
-	    	break;
-	    case html.includes("UA-9296528-13"):
+			break;
+		case html.includes("UA-9296528-13"):
 			city += 'Rhinelander';
-	        break;
-	    case html.includes("UA-9296528-39"):
+			break;
+		case html.includes("UA-9296528-39"):
 			city += 'West Bend';
-	        break;
-	    case html.includes("UA-9296528-21"):
+			break;
+		case html.includes("UA-9296528-21"):
 			city += 'Vilas County';
-	        break;
-	    case html.includes("UA-9296528-8"):
+			break;
+		case html.includes("UA-9296528-8"):
 			city += 'Oneida County';
-	        break;
-	    case html.includes("UA-9296528-4"):
+			break;
+		case html.includes("UA-9296528-4"):
 			city += 'Wisconsin Travel Best Bets';
-	        break;
-	    case html.includes("UA-9296528-10"):
+			break;
+		case html.includes("UA-9296528-10"):
 			city += 'Boulder Junction';
-	        break;
-	    case html.includes("UA-3678323-79"):
+			break;
+		case html.includes("UA-3678323-79"):
 			city += 'Fitchburg';
 			break;
 		case html.includes("'UA-9296528-22'"):
@@ -47,13 +60,16 @@ function DOMtoString(document_root) {
 		case html.includes("UA-25665374-1"):
 			city += 'Tomahawk';
 			break;
-	     default:
-	     	city += 'nothing for P&B';
-	     }
+		default:
+				city += 'nothing for P&B';
+	}
+
 	var count = (html.match(/UA-/g) || [] ).length;
+
 	return "The code is for " + city + '\n'
 		+ "GA tag was called " + count + " times." + '\n'
-	 	+ " Target is 2 times";
+		+ "Target is 2 times:" + '\n'
+		+ googleCodeLines;
 }
 
 chrome.runtime.sendMessage({
